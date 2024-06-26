67b1d955b83f5ec0273431d9d8f9d7e39f94f93f
MemanAwad <meman1awad@gmail.com>
2024-06-27 01:55:37 +0300

    task0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" BasicCache that inherits from BaseCaching """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''implement cache '''
+
+    def __init__(self):
+        '''init function'''
+        super().__init__()
+
+
+    def put(self, key, item):
+        '''put in the cache dictionary'''
+        if key == None or item == None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        ''' get from  the cache dictionary'''
+        if key == None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
